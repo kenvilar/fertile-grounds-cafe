1a0016c950489c5a41a1355bebc3427725e393c2
Ken Vilar <kenvilar@gmail.com>
2017-11-18 21:18:35 +0800

    Remove default style.css in a function

--- inc/styles.php
+++ inc/styles.php
@@ -4,7 +4,6 @@
  * Enqueue styles.
  */
 function fertilegroundscafe_styles() {
-	wp_enqueue_style( 'fertilegroundscafe-style', get_stylesheet_uri() );
 }
 
 add_action( 'wp_enqueue_scripts', 'fertilegroundscafe_styles' );
