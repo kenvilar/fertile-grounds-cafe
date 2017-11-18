c3bdeaddf90b3bb437fcf3881f03e3a62d279fcd
Ken Vilar <kenvilar@gmail.com>
2017-11-18 20:55:19 +0800

    Move fonts and js to assets folder

--- functions.php
+++ functions.php
@@ -119,9 +119,9 @@ add_action( 'widgets_init', 'fertilegroundscafe_widgets_init' );
 function fertilegroundscafe_scripts() {
 	wp_enqueue_style( 'fertilegroundscafe-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'fertilegroundscafe-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'fertilegroundscafe-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'fertilegroundscafe-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'fertilegroundscafe-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
