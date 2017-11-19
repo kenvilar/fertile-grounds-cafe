73dbb655611b95a74dd6aba19632cd1ec552ec3a
Ken Vilar <kenvilar@gmail.com>
2017-11-19 19:44:33 +0800

    Register expanded and compressed styles

--- inc/styles.php
+++ inc/styles.php
@@ -4,6 +4,14 @@
  * Enqueue styles.
  */
 function fertilegroundscafe_styles() {
+	wp_enqueue_style( 'main-style-expanded', get_template_directory_uri() . '/assets/css/style.css',
+		'',
+		'2017',
+		'all' );
+	wp_enqueue_style( 'main-style-compressed', get_template_directory_uri() . '/assets/css/style.min.css',
+		'',
+		'2017',
+		'all' );
 }
 
 add_action( 'wp_enqueue_scripts', 'fertilegroundscafe_styles' );
