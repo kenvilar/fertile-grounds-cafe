27929e2566a9a3eb99ce39eae52ba9036a22b6ca
Ken Vilar <kenvilar@gmail.com>
2017-11-20 22:49:59 +0800

    Update footer

--- footer.php
+++ footer.php
@@ -4,29 +4,14 @@
  *
  * Contains the closing of the #content div and all content after.
  *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ * @link    https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
  * @package Fertile_Grounds_Cafe
  */
 
 ?>
 
-	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'fertilegroundscafe' ) ); ?>"><?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'fertilegroundscafe' ), 'WordPress' );
-			?></a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'fertilegroundscafe' ), 'fertilegroundscafe', '<a href="#">kenvilar</a>' );
-			?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+</div><!-- #wrapper -->
 
 <?php wp_footer(); ?>
 
