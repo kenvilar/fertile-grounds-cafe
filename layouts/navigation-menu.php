86ee4cb68062dee68643591ccff99fb6059c2b7c
Ken Vilar <kenvilar@gmail.com>
2017-11-22 01:30:38 +0800

    No padding

--- layouts/navigation-menu.php
+++ layouts/navigation-menu.php
@@ -7,17 +7,17 @@
 	<div class="intro-content">
 		<div class="container">
 			<div class="row">
-				<div class="col-lg-8 col-md-12 col-sm-12 col-xs-12">
+				<div class="col-lg-8 col-md-10 col-sm-8 col-xs-12 nopadding">
 					<h1>Welcome to <br>Fertile Grounds Cafe!</h1>
 				</div>
 			</div>
 		</div>
-		<div class="col-lg-7 col-md-12 col-sm-12 col-xs-12" style="padding-left: 0;">
+		<div class="col-lg-7 col-md-12 col-sm-12 col-xs-12 nopadding" style="padding-left: 0;">
 			<hr>
 		</div>
 		<div class="container">
 			<div class="row">
-				<div class="col-lg-8 col-md-12 col-sm-12 col-xs-12">
+				<div class="col-lg-8 col-md-12 col-sm-12 col-xs-12 nopadding">
 					<h4>Coffee House <span class="text-brown">•</span> Wellness Shop <span class="text-brown">•</span>
 						Events <span class="text-brown">•</span> Rental Space
 					</h4>
