cefe571d6bfb959ee56b9114cf0c086fb80a392d
Ken Vilar <kenvilar@gmail.com>
2017-11-22 02:35:02 +0800

    Customize new button

--- front-page.php
+++ front-page.php
@@ -4,7 +4,35 @@ get_header();
 ?>
 
 	<section id="home-about">
+		<div class="container">
+			<div class="row">
+				<div class="nopadding col-lg-6">
+					<img class="img-responsive"
+					     src="<?php echo get_template_directory_uri() . '/assets/images/home/mug.png'; ?>"
+					     alt="home-image">
+				</div>
+				<div class="nopadding col-lg-6">
+					<br>
+					<h2 class="text-brown text-uppercase">About</h2>
+					<div class="h6">
+						Fertile Grounds Cafe is located in one of Rochester's treasured East Avenue mansions. We serve
+						high-quality espresso drinks, loose leaf teas and real fruit smoothies, as well as a
+						health-conscious selection of light fare.
 
+						<br><br>
+						
+						Completely remodeled, our coffee shop boasts multiple gathering spaces, including gardens,
+						patios, decks and cozy fireplaces, perfect for small groups or solitude. Inside, you will find
+						an eclectic collection of wellness-centered goods, including essential oils, self-help books,
+						journals... even premium bedding.
+
+						<br><br>
+
+						<a href="" class="btn btn-version1">Learn more &nbsp;<i class="fa fa-angle-right"></i></a>
+					</div>
+				</div>
+			</div>
+		</div>
 	</section>
 
 <?php
