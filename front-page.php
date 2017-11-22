8777887207e481b5666444388fa81336fdbc6e9d
Ken Vilar <kenvilar@gmail.com>
2017-11-22 14:29:11 +0800

    Front page venue section

--- front-page.php
+++ front-page.php
@@ -35,5 +35,44 @@ get_header();
 		</div>
 	</section>
 
+	<section id="home-venue">
+		<div class="container-fluid">
+			<div class="col-lg-6 col-md-6 col-sm-12" style="background: #937a6b;padding-top: 50px;">
+				<div class="col-lg-offset-3 col-lg-9 col-md-offset-1 col-md-11 nopadding">
+					<h2 class="text-uppercase text-white">Venue</h2>
+					<div class="h6 text-white" style="width: 97%;">
+						Looking for a special place to host a shower, meeting, workshop or party?
+
+						<br><br>
+
+						Fertile Grounds Cafe offers a variety of different seasonal venue spaces. In the warmer
+						months, multiple decks, a patio, and a large park/garden space are available. In the cooler
+						months, we have a café, a fireplace lounge area and a large yoga studio space. Come tour our
+						amazing facility and its grounds, and choose the space that’s right for you!
+
+						<br><br>
+
+						Small groups of 10-20 people are a perfect fit for our intimate space, but we can accommodate
+						up to 40.
+
+						<br><br>
+
+						We offer cafe service of tea, coffee, and light fare, or you are welcome to bring in your own
+						caterer. Or, if you prefer, describe to us what you’re looking for, and we will be happy to
+						work with a caterer on your behalf.
+
+						<br><br>
+
+						<a href="" class="btn btn-version2">Check Rental Fees &nbsp;<i
+									class="fa fa-angle-right"></i></a>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-5 col-md-5">
+				<!--put here the gallery-->
+			</div>
+		</div>
+	</section>
+
 <?php
 get_footer();
