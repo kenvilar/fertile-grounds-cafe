c1757efb4b7759a96aa99b3e69f22ab5611f6eea
Ken Vilar <kenvilar@gmail.com>
2017-11-23 06:08:11 +0800

    Fix for theme template file caching from Wordpress version 4.9 bug

--- functions.php
+++ functions.php
@@ -157,3 +157,19 @@ if ( class_exists( 'WooCommerce' ) ) {
  * Own Custom
  */
 require get_template_directory() . '/inc/custom.php';
+
+function fix_template_caching( WP_Screen $current_screen ) {
+	if ( ! in_array( $current_screen->base, array( 'post', 'edit', 'theme-editor' ), true ) ) {
+		return;
+	}
+	$theme = wp_get_theme();
+	if ( ! $theme ) {
+		return;
+	}
+	$cache_hash    = md5( $theme->get_theme_root() . '/' . $theme->get_stylesheet() );
+	$label         = sanitize_key( 'files_' . $cache_hash . '-' . $theme->get( 'Version' ) );
+	$transient_key = substr( $label, 0, 29 ) . md5( $label );
+	delete_transient( $transient_key );
+}
+
+add_action( 'current_screen', 'fix_template_caching' );
