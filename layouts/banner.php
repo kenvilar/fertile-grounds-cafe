be323ffbc14fbf7a323c7ed6f2bd4ca6380200a8
Ken Vilar <kenvilar@gmail.com>
2017-11-23 16:27:26 +0800

    Fix banner

--- layouts/banner.php
+++ layouts/banner.php
@@ -5,10 +5,12 @@
 <section id="Banner" data-image="<?php echo get_template_directory_uri() . '/assets/images/banner/home.png'; ?>"
          style="background-image: url(<?php echo get_template_directory_uri() . '/assets/images/banner/home.png'; ?>);">
 	<div class="intro-content">
+
 		<div class="container">
 			<div class="row">
-				<div class="col-lg-8 col-md-10 col-sm-8 col-xs-12 nopadding">
-					<?php if ( is_home() || is_front_page() ) : ?>
+				
+				<?php if ( is_home() || is_front_page() ) : ?>
+					<div class="col-lg-8 col-xs-12 nopadding">
 						<h1 class="animated fadeInUp home-banner-title">
 							<?php
 							if ( get_field( 'main_page_title' ) ) {
@@ -16,7 +18,9 @@
 							}
 							?>
 						</h1>
-					<?php else : ?>
+					</div>
+				<?php else : ?>
+					<div class="col-lg-4 col-xs-12 nopadding">
 						<h1 class="animated fadeInUp banner-title-light">
 							<?php
 							if ( get_field( 'main_page_title' ) ) {
@@ -24,13 +28,22 @@
 							}
 							?>
 						</h1>
-					<?php endif; ?>
-				</div>
+					</div>
+				<?php endif; ?>
+
 			</div>
 		</div>
-		<div class="col-lg-8 col-md-12 col-sm-12 col-xs-12 nopadding" style="padding-left: 0;">
-			<hr>
-		</div>
+		
+		<?php if ( is_home() || is_front_page() ) : ?>
+			<div class="col-lg-8 col-xs-12 nopadding" style="padding-left: 0;">
+				<hr>
+			</div>
+		<?php else : ?>
+			<div class="col-lg-4 col-xs-12 nopadding" style="padding-left: 0;">
+				<hr>
+			</div>
+		<?php endif; ?>
+		
 		<?php if ( is_home() || is_front_page() ) : ?>
 			<div class="container">
 				<div class="row">
@@ -43,5 +56,6 @@
 				</div>
 			</div>
 		<?php endif; ?>
+
 	</div>
 </section>
