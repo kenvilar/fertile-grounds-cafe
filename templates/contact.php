ec7049dbd4f1ab4fa445b96f47d320f01475331c
Ken Vilar <kenvilar@gmail.com>
2017-11-25 02:27:43 +0800

    Embed map fertile grounds cafe location

--- templates/contact.php
+++ templates/contact.php
@@ -15,6 +15,9 @@ get_header(); ?>
 						<div class="map"
 						     data-image="<?php echo get_template_directory_uri() . '/assets/images/contact/map.png'; ?>"
 						     style="height: 615px;background-image: url(<?php echo get_template_directory_uri() . '/assets/images/contact/map.png'; ?>)">
+							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2911.244095302665!2d-77.54700468510575!3d43.14140329339266!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d6ca8e9b975b89%3A0x4a3512a561ffcf50!2s2244+East+Ave%2C+Rochester%2C+NY+14610%2C+USA!5e0!3m2!1sen!2sph!4v1511547762410"
+							        width="100%" height="100%" frameborder="0" style="border:0"
+							        allowfullscreen></iframe>
 						</div>
 						<!--black background-->
 						<div class="address background-black text-white">
@@ -66,7 +69,7 @@ get_header(); ?>
 				<div class="col-xs-12 col-md-6">
 					<div class="contact-form-wrapper">
 						<h2 class="text-uppercase text-brown no-margin">Let's Talk</h2>
-						
+
 						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pretium quam ornare
 							hendrerit tristique. Duis quis felis ac neque accumsan volutpat. Curabitur gravida tempus
 							lacinia. Donec vel massa elit. Proin ac felis erat. Donec quis turpis sit amet lacus posuere
@@ -96,7 +99,8 @@ get_header(); ?>
 								</div>
 								<div class="form-row">
 									<div class="form-group">
-										<button class="h4 background-black btn btn-contact" type="submit">Submit</button>
+										<button class="h4 background-black btn btn-contact" type="submit">Submit
+										</button>
 									</div>
 								</div>
 							</form>
