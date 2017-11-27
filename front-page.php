cd20b1bfc37bead89eaa01f04e0f4513176374f7
Ken Vilar <kenvilar@gmail.com>
2017-11-28 03:45:32 +0800

    Front page fix paragraphs

--- front-page.php
+++ front-page.php
@@ -15,18 +15,14 @@ get_header();
 					<br>
 					<h2 class="text-brown text-uppercase">About</h2>
 					<div class="h6">
-						Fertile Grounds Cafe is located in one of Rochester's treasured East Avenue mansions. We serve
-						high-quality espresso drinks, loose leaf teas and real fruit smoothies, as well as a
-						health-conscious selection of light fare.
+						<p>Fertile Grounds Cafe is located in one of Rochester's treasured East Avenue mansions. We
+							serve high-quality espresso drinks, loose leaf teas and real fruit smoothies, as well as a
+							health-conscious selection of light fare.</p>
 
-						<br><br>
-
-						Completely remodeled, our coffee shop boasts multiple gathering spaces, including gardens,
-						patios, decks and cozy fireplaces, perfect for small groups or solitude. Inside, you will find
-						an eclectic collection of wellness-centered goods, including essential oils, self-help books,
-						journals... even premium bedding.
-
-						<br><br>
+						<p>Completely remodeled, our coffee shop boasts multiple gathering spaces, including gardens,
+							patios, decks and cozy fireplaces, perfect for small groups or solitude. Inside, you will
+							find an eclectic collection of wellness-centered goods, including essential oils, self-help
+							books, journals... even premium bedding.</p>
 
 						<a href="/about" class="btn btn-version1">Learn more &nbsp;<i class="fa fa-angle-right"></i></a>
 					</div>
@@ -41,27 +37,19 @@ get_header();
 				<div class="col-lg-offset-3 col-lg-9 col-md-offset-1 col-md-11 nopadding">
 					<h2 class="text-uppercase text-white">Venue</h2>
 					<div class="h6 text-white" style="width: 97%;">
-						Looking for a special place to host a shower, meeting, workshop or party?
+						<p>Looking for a special place to host a shower, meeting, workshop or party?</p>
 
-						<br><br>
+						<p>Fertile Grounds Cafe offers a variety of different seasonal venue spaces. In the warmer
+							months, multiple decks, a patio, and a large park/garden space are available. In the cooler
+							months, we have a café, a fireplace lounge area and a large yoga studio space. Come tour our
+							amazing facility and its grounds, and choose the space that’s right for you!</p>
 
-						Fertile Grounds Cafe offers a variety of different seasonal venue spaces. In the warmer
-						months, multiple decks, a patio, and a large park/garden space are available. In the cooler
-						months, we have a café, a fireplace lounge area and a large yoga studio space. Come tour our
-						amazing facility and its grounds, and choose the space that’s right for you!
+						<p>Small groups of 10-20 people are a perfect fit for our intimate space, but we can accommodate
+							up to 40.</p>
 
-						<br><br>
-
-						Small groups of 10-20 people are a perfect fit for our intimate space, but we can accommodate
-						up to 40.
-
-						<br><br>
-
-						We offer cafe service of tea, coffee, and light fare, or you are welcome to bring in your own
-						caterer. Or, if you prefer, describe to us what you’re looking for, and we will be happy to
-						work with a caterer on your behalf.
-
-						<br><br>
+						<p>We offer cafe service of tea, coffee, and light fare, or you are welcome to bring in your own
+							caterer. Or, if you prefer, describe to us what you’re looking for, and we will be happy to
+							work with a caterer on your behalf.</p>
 
 						<a href="/venue" class="btn btn-version2">Check Rental Fees &nbsp;<i
 									class="fa fa-angle-right"></i></a>
