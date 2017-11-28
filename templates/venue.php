57d08309fd457c821e0e72f99befaeb043588894
Ken Vilar <kenvilar@gmail.com>
2017-11-28 21:02:28 +0800

    Integrate venue template page with WP custom fields

--- templates/venue.php
+++ templates/venue.php
@@ -8,26 +8,20 @@ get_header(); ?>
 	<section>
 		<div class="container-fluid nopadding">
 			<div class="col-md-6 col-xs-12 nopadding hidden-sm hidden-xs"
-			     data-image="<?php echo get_template_directory_uri() . '/assets/images/venue/venue1.png'; ?>"
-			     style="height: 859px;background-image: url(<?php echo get_template_directory_uri() . '/assets/images/venue/venue1.png'; ?>)">
+			     data-image="<?php
+			     echo get_field( 'first_section_image' ) ? the_field( 'first_section_image' ) : get_template_directory_uri() . '/assets/images/venue/venue1.png';
+			     ?>"
+			     style="height: 859px;background-image: url(<?php
+			     echo get_field( 'first_section_image' ) ? the_field( 'first_section_image' ) : get_template_directory_uri() . '/assets/images/venue/venue1.png';
+			     ?>)">
 
 			</div>
 			<div class="col-md-5 col-xs-12">
-				<h2 class="text-uppercase text-brown no-margin" style="line-height: 80px;">
-					Looking for a special place to host a shower, meeting, workshop or party?
-				</h2>
-				<br>
-				<p>Fertile Grounds Cafe offers a variety of different seasonal venue spaces. In the warmer months,
-					multiple decks, a patio, and a large park/garden space are available. In the cooler months, we have
-					a café, a fireplace lounge area and a large yoga studio space. Come tour our amazing facility and
-					its grounds, and choose the space that’s right for you!</p>
-				<br>
-				<p>Small groups of 10-20 people are a perfect fit for our intimate space, but we can accommodate up to
-					40.</p>
-				<br>
-				<p>We offer cafe service of tea, coffee, and light fare, or you are welcome to bring in your own
-					caterer. Or, if you prefer, describe to us what you’re looking for, and we will be happy to work
-					with a caterer on your behalf.</p>
+				<?php
+				if ( get_field( 'first_section_content' ) ) {
+					the_field( 'first_section_content' );
+				}
+				?>
 			</div>
 		</div>
 	</section>
@@ -35,16 +29,11 @@ get_header(); ?>
 	<section>
 		<div class="container">
 			<div class="row">
-				<div class="col-xs-12 col-md-6 nopadding">
-					<h2 class="text-uppercase text-brown">Fees:</h2>
-					<p>$100/hour space rental, two hour minimum. This includes unlimited coffee, tea and water for your
-						guests.</p>
-				</div>
-				<div class="col-xs-12 col-md-6">
-					<h2 class="text-uppercase text-brown">Add ons:</h2>
-					<p>$35/person, charcuterie boards (cured meats, olives, cheese, bread. etc.) <br>
-						$20 party corkage fee, limit one bottle per person.</p>
-				</div>
+				<?php
+				if ( get_field( 'second_section_content' ) ) {
+					the_field( 'second_section_content' );
+				}
+				?>
 			</div>
 		</div>
 	</section>
