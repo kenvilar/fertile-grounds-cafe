91aa2864796405617d26200b365e47cc398f876e
Ken Vilar <kenvilar@gmail.com>
2017-11-28 22:17:12 +0800

    Add customize WP fields for contact template

--- templates/contact.php
+++ templates/contact.php
@@ -5,7 +5,7 @@
 
 get_header(); ?>
 
-	<section>
+	<section style="padding-top: 80px;padding-bottom: 90px;">
 		<div class="container">
 			<div class="row">
 
@@ -15,9 +15,14 @@ get_header(); ?>
 						<div class="map"
 						     data-image="<?php echo get_template_directory_uri() . '/assets/images/contact/map.png'; ?>"
 						     style="height: 615px;background-image: url(<?php echo get_template_directory_uri() . '/assets/images/contact/map.png'; ?>)">
-							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2911.244095302665!2d-77.54700468510575!3d43.14140329339266!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d6ca8e9b975b89%3A0x4a3512a561ffcf50!2s2244+East+Ave%2C+Rochester%2C+NY+14610%2C+USA!5e0!3m2!1sen!2sph!4v1511547762410"
-							        width="100%" height="100%" frameborder="0" style="border:0"
-							        allowfullscreen></iframe>
+							<?php
+							if ( get_field( 'map_url' ) ) {
+								?>
+								<iframe src="<?php the_field( 'map_url' ); ?>" width="100%" height="100%"
+								        frameborder="0" style="border:0" allowfullscreen></iframe>
+								<?php
+							}
+							?>
 						</div>
 						<!--black background-->
 						<div class="address background-black text-white">
@@ -30,8 +35,11 @@ get_header(); ?>
 										</div>
 										<div class="col-xs-12 col-sm-4 nopadding">
 											<span class="h6">
-												2244 East Avenue <br>
-												Rochester, New York
+												<?php
+												if ( get_field( 'address' ) ) {
+													the_field( 'address' );
+												}
+												?>
 											</span>
 										</div>
 
@@ -43,7 +51,14 @@ get_header(); ?>
 												</div>
 												<div class="col-xs-12 col-sm-10">
 													<span class="h6">
-														<a class="link-no-hover" href="tel:+1 585-244-1280">+1 585-244-1280</a>
+														<a class="link-no-hover"
+														   href="tel:<?php if ( get_field( 'phone' ) ) {
+															   the_field( 'phone' );
+														   } ?>">
+															<?php if ( get_field( 'phone' ) ) {
+																the_field( 'phone' );
+															} ?>
+														</a>
 													</span>
 												</div>
 											</div>
@@ -54,7 +69,13 @@ get_header(); ?>
 												<div class="col-xs-12 col-sm-10">
 													<span class="h6">
 														<a class="link-no-hover"
-														   href="mailto:info@fertilegroundscafe.com">info@fertilegroundscafe.com</a>
+														   href="mailto:<?php if ( get_field( 'email' ) ) {
+															   the_field( 'email' );
+														   } ?>">
+															<?php if ( get_field( 'email' ) ) {
+																the_field( 'email' );
+															} ?>
+														</a>
 													</span>
 												</div>
 											</div>
@@ -69,11 +90,16 @@ get_header(); ?>
 				<div class="col-xs-12 col-md-6">
 					<div class="contact-form-wrapper">
 						<h2 class="text-uppercase text-brown no-margin">Let's Talk</h2>
-
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pretium quam ornare
-							hendrerit tristique. Duis quis felis ac neque accumsan volutpat. Curabitur gravida tempus
-							lacinia. Donec vel massa elit. Proin ac felis erat. Donec quis turpis sit amet lacus posuere
-							mattis in at risus. Pellentesque at malesuada tortor.</p>
+						
+						<?php
+						if ( have_posts() ) :
+							while ( have_posts() ) :
+								the_post();
+								the_content();
+							endwhile;
+							wp_reset_postdata();
+						endif;
+						?>
 						<div class="contact-form-box">
 							<?php
 							if ( get_field( 'contact_form_shortcode' ) ) {
