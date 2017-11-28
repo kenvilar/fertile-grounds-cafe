8d624d9bd7ee6e8a4705c36abc7ede25a18916d1
Ken Vilar <kenvilar@gmail.com>
2017-11-28 21:06:53 +0800

    Fix first_section_image custom field

--- templates/venue.php
+++ templates/venue.php
@@ -7,16 +7,22 @@ get_header(); ?>
 
 	<section>
 		<div class="container-fluid nopadding">
-			<div class="col-md-6 col-xs-12 nopadding hidden-sm hidden-xs"
-			     data-image="<?php
-			     echo get_field( 'first_section_image' ) ? the_field( 'first_section_image' ) : get_template_directory_uri() . '/assets/images/venue/venue1.png';
-			     ?>"
-			     style="height: 859px;background-image: url(<?php
-			     echo get_field( 'first_section_image' ) ? the_field( 'first_section_image' ) : get_template_directory_uri() . '/assets/images/venue/venue1.png';
-			     ?>)">
+			<?php
+			if ( get_field( 'first_section_image' ) ) :
+				?>
+				<div class="col-md-6 col-xs-12 nopadding hidden-sm hidden-xs"
+				     data-image="<?php
+				     echo get_field( 'first_section_image' ) ? the_field( 'first_section_image' ) : get_template_directory_uri() . '/assets/images/venue/venue1.png';
+				     ?>"
+				     style="height: 859px;background-image: url(<?php
+				     echo get_field( 'first_section_image' ) ? the_field( 'first_section_image' ) : get_template_directory_uri() . '/assets/images/venue/venue1.png';
+				     ?>)">
 
-			</div>
-			<div class="col-md-5 col-xs-12">
+				</div>
+				<?php
+			endif;
+			?>
+			<div class="<?php echo get_field( 'first_section_image' ) ? 'col-md-5' : 'col-md-12'; ?> col-xs-12">
 				<?php
 				if ( get_field( 'first_section_content' ) ) {
 					the_field( 'first_section_content' );
