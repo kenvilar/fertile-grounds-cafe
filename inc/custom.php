8369183f9fa1f6e4ff3dab149e47430b703996c7
Ken Vilar <kenvilar@gmail.com>
2017-11-28 22:29:02 +0800

    acf options settings

--- inc/custom.php
+++ inc/custom.php
@@ -30,3 +30,28 @@ add_action( 'acf/init', 'acf_wysiwyg_remove_wpautop', 15 );
 function acf_wysiwyg_remove_wpautop() {
 	remove_filter( 'acf_the_content', 'wpautop' );
 }
+
+// ACF Options Page
+if ( function_exists( 'acf_add_options_page' ) ) {
+	
+	acf_add_options_page( array(
+		'page_title' => 'Theme General Settings',
+		'menu_title' => 'Theme Settings',
+		'menu_slug'  => 'theme-general-settings',
+		'capability' => 'edit_posts',
+		'redirect'   => false,
+	) );
+	
+	acf_add_options_sub_page( array(
+		'page_title'  => 'Theme Header Settings',
+		'menu_title'  => 'Header',
+		'parent_slug' => 'theme-general-settings',
+	) );
+	
+	acf_add_options_sub_page( array(
+		'page_title'  => 'Theme Footer Settings',
+		'menu_title'  => 'Footer',
+		'parent_slug' => 'theme-general-settings',
+	) );
+	
+}
