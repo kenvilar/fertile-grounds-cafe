64f8a9ada2e24146c6f03cbb49a45473e5025b64
Ken Vilar <kenvilar@gmail.com>
2017-11-29 21:37:55 +0800

    Add favicon and create logic conditional statement

--- header.php
+++ header.php
@@ -16,6 +16,13 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="icon" href="<?php
+	if ( get_field( 'favicon', 'option' ) ) {
+		the_field( 'favicon', 'option' );
+	} else {
+		echo get_template_directory_uri() . '/assets/images/logos/favicon.png';
+	}
+	?>" sizes="any" type="image/png">
 	
 	<?php wp_head(); ?>
 </head>
