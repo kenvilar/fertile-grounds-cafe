3e866792c5cd50fa4c031c98744313596212dab2
Ken Vilar <kenvilar@gmail.com>
2017-12-01 00:42:15 +0800

    Revert back to manual menu code

--- layouts/navigation-menu.php
+++ layouts/navigation-menu.php
@@ -33,7 +33,7 @@
 				</button>
 			</div>
 			<?php
-			wp_nav_menu( array(
+			/*wp_nav_menu( array(
 				'menu'            => 'primary-menu',
 				'container'       => 'div',
 				'container_id'    => 'navbar',
@@ -46,8 +46,27 @@
 				'depth'           => 0,
 				'walker'          => new Fertilegrounds_Walker_Nav_Menu(),
 				'theme_location'  => 'primary-menu',
-			) );
+			) );*/
 			?>
+			<div id="navbar" class="collapse navbar-collapse">
+				<ul class="nav navbar-nav navbar-right">
+					<li class=""><a href="http://fertilegrounds.dev/about/">About</a></li>
+					<li class="has-children">
+						<a href="http://fertilegrounds.dev/menu/">Menu
+							<span data-toggle="dropdown" class="dropdown-toggle glyphicon glyphicon-chevron-down"></span></a>
+						<ul class="dropdown-menu" role="menu">
+							<li class=""><a class="light-fare" href="http://fertilegrounds.dev/menu/#light-fare">Light Fare</a></li>
+							<li class=""><a class="menu-tab" href="http://fertilegrounds.dev/menu/#coffee">Coffee</a></li>
+							<li class=""><a class="menu-tab" href="http://fertilegrounds.dev/menu/#drinks">Drinks</a></li>
+							<li class=""><a class="menu-tab" href="http://fertilegrounds.dev/menu/#teas">Teas</a></li>
+							<li class=""><a class="menu-tab" href="http://fertilegrounds.dev/menu/#smoothies">Smoothies</a></li>
+						</ul>
+					</li>
+					<li class=""><a href="http://fertilegrounds.dev/venue/">Venue</a></li>
+					<li class=""><a href="/events">Events</a></li>
+					<li class=""><a href="http://fertilegrounds.dev/contact-us/">Contact Us</a></li>
+				</ul>
+			</div>
 		</div>
 	</nav>
 </header>
