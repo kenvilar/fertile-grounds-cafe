4fcdce115380cf51527ebe671a301531c8d42350
Ken Vilar <kenvilar@gmail.com>
2017-11-30 16:46:07 +0800

    Update new teas menu with content

--- template-parts/menu/teas.php
+++ template-parts/menu/teas.php
@@ -10,13 +10,15 @@
 <div class="col-xs-12 col-md-6 panel-body">
 	<span class="h2 text-brown text-bold text-uppercase">Teas and Tisane</span>
 	<div class="clear"></div>
-	<span class="text-black h6 text-medium-italic">Made with frozen fruit, yogurt / milk, and or fruit juice.</span>
+	<h3 class="text-brown text-uppercase"><strong>Serendipitea: Organic Teas and Tisane</strong></h3>
+	<span class="text-black h6 text-medium-italic">Served Hot or Iced</span>
 	<table class="table table-responsive">
 		<thead>
 
 		<tr>
 			<th></th>
 			<th class="text-brown">Sm</th>
+			<th class="text-brown">Med</th>
 			<th class="text-brown">Lrg</th>
 		</tr>
 
@@ -26,56 +28,111 @@
 		<!--Menu Item-->
 		<tr>
 			<td>
-				<span class="table-title"><strong>Caribbean Coolada</strong></span>
+				<span class="table-title"><strong>China Green</strong></span>
 				<div class="clearfix"></div>
-				Organic vanilla greek yogurt, strawberry, pineapple, banana, mangos
 			</td>
-			<td><strong>3.50</strong></td>
-			<td><strong>5.75</strong></td>
+			<td><strong>1.25</strong></td>
+			<td><strong>1.75</strong></td>
+			<td><strong>2.50</strong></td>
 		</tr>
 
 		<!--Menu Item-->
 		<tr>
 			<td>
-				<span class="table-title"><strong>Peachy Breakfast</strong></span>
+				<span class="table-title"><strong>Decaf Green*</strong></span>
 				<div class="clearfix"></div>
-				Organic vanilla greek yogurt, apple juice, banana, peaches, blackberry
 			</td>
-			<td><strong>4.25</strong></td>
-			<td><strong>6.50</strong></td>
+			<td><strong>1.25</strong></td>
+			<td><strong>1.75</strong></td>
+			<td><strong>2.50</strong></td>
 		</tr>
 
 		<!--Menu Item-->
 		<tr>
 			<td>
-				<span class="table-title"><strong>Yoga Blend</strong> <i>(Dairy Free)</i></span>
+				<span class="table-title"><strong>Pai Mu Tan White Tea</strong></span>
 				<div class="clearfix"></div>
-				Strawberries, mangos, guava nectar, banana
 			</td>
-			<td><strong>3.50</strong></td>
-			<td><strong>5.75</strong></td>
+			<td><strong>1.25</strong></td>
+			<td><strong>1.75</strong></td>
+			<td><strong>2.50</strong></td>
 		</tr>
 
 		<!--Menu Item-->
 		<tr>
 			<td>
-				<span class="table-title"><strong>Berry Blast</strong> <i>(Dairy Free)</i></span>
+				<span class="table-title"><strong>Earl Grey Organic Black Tea & Bergamot</strong></span>
 				<div class="clearfix"></div>
-				Cranberry juice, strawberries, blueberries, raspberries, blackberries
 			</td>
-			<td><strong>4.25</strong></td>
-			<td><strong>6.50</strong></td>
+			<td><strong>1.25</strong></td>
+			<td><strong>1.75</strong></td>
+			<td><strong>2.50</strong></td>
 		</tr>
 
 		<!--Menu Item-->
 		<tr>
 			<td>
-				<span class="table-title"><strong>Chocolate Craver</strong></span>
+				<span class="table-title"><strong>Berry Blueberry*</strong></span>
 				<div class="clearfix"></div>
-				Organic peanut butter, banana, almonds, Ghirardelli chocolate, vanilla soy
 			</td>
-			<td><strong>3.50</strong></td>
-			<td><strong>5.75</strong></td>
+			<td><strong>1.25</strong></td>
+			<td><strong>1.75</strong></td>
+			<td><strong>2.50</strong></td>
+		</tr>
+
+		<!--Menu Item-->
+		<tr>
+			<td>
+				<span class="table-title"><strong>Ruby Slipper*</strong></span>
+				<div class="clearfix"></div>
+			</td>
+			<td><strong>1.25</strong></td>
+			<td><strong>1.75</strong></td>
+			<td><strong>2.50</strong></td>
+		</tr>
+
+		<!--Menu Item-->
+		<tr>
+			<td>
+				<span class="table-title"><strong>Strictly Slipper*</strong></span>
+				<div class="clearfix"></div>
+			</td>
+			<td><strong>1.25</strong></td>
+			<td><strong>1.75</strong></td>
+			<td><strong>2.50</strong></td>
+		</tr>
+
+		<!--Menu Item-->
+		<tr>
+			<td>
+				<span class="table-title"><strong>Eve's Temptation*</strong></span>
+				<div class="clearfix"></div>
+			</td>
+			<td><strong>1.25</strong></td>
+			<td><strong>1.75</strong></td>
+			<td><strong>2.50</strong></td>
+		</tr>
+
+		<!--Menu Item-->
+		<tr>
+			<td>
+				<span class="table-title"><strong>Really Raspberry*</strong></span>
+				<div class="clearfix"></div>
+			</td>
+			<td><strong>1.25</strong></td>
+			<td><strong>1.75</strong></td>
+			<td><strong>2.50</strong></td>
+		</tr>
+
+		<!--Menu Item-->
+		<tr>
+			<td>
+				<span class="table-title"><strong>Moontime Tea: Fertility Blend*</strong></span>
+				<div class="clearfix"></div>
+			</td>
+			<td><strong>1.25</strong></td>
+			<td><strong>1.75</strong></td>
+			<td><strong>2.50</strong></td>
 		</tr>
 
 		</tbody>
