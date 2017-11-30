80d23d39d835d17f7c6882a39f408a7a81afc2d8
Ken Vilar <kenvilar@gmail.com>
2017-11-30 15:17:35 +0800

    Create webpack configuration for minifying custom js file

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require( 'webpack' );
+
+module.exports = {
+	context : __dirname,
+	devtool : debug ? "inline-sourcemap" : false,
+	entry   : "./assets/js/custom.js",
+	output  : {
+		path     : __dirname + "/assets/js",
+		filename : "custom.min.js"
+	},
+	plugins : debug ? [] : [
+		new webpack.optimize.OccurrenceOrderPlugin(),
+		new webpack.optimize.UglifyJsPlugin( { mangle : false, sourcemap : false } )
+	]
+};
