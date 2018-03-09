5bf7c586e12ed2d8d4ebba894d544e5c2ca117ed
Ken Vilar <kenvilar@gmail.com>
2018-03-09 22:13:54 +0800

    Make the tee manu description a two columns

--- template-parts/menu/teas.php
+++ template-parts/menu/teas.php
@@ -7,7 +7,7 @@
 	     style="height: 475px;background-image: url(<?php echo get_template_directory_uri() . '/assets/images/menu/new-teas.jpg'; ?>)">
 	</div>
 </div>
-<div class="col-xs-12 col-md-6 panel-body">
+<div class="col-xs-12 col-md-8 panel-body">
 	<span class="h2 text-brown text-bold text-uppercase">Teas and Tisane</span>
 	<div class="clear"></div>
 	<h3 class="text-brown text-uppercase"><strong>Serendipitea: Organic Teas and Tisane</strong></h3>
@@ -20,134 +20,95 @@
 
 		<tr>
 			<th></th>
+			<th></th>
 		</tr>
 
 		</thead>
 		<tbody>
 
-		<!--Menu Item-->
 		<tr>
 			<td>
 				<span class="table-title"><strong>China Green</strong></span>
 				<div class="clearfix"></div>
 			</td>
-		</tr>
-
-		<!--Menu Item-->
-		<tr>
 			<td>
 				<span class="table-title"><strong>Decaf Green*</strong></span>
 				<div class="clearfix"></div>
 			</td>
 		</tr>
 
-		<!--Menu Item-->
 		<tr>
 			<td>
 				<span class="table-title"><strong>Pai Mu Tan White Tea</strong></span>
 				<div class="clearfix"></div>
 			</td>
-		</tr>
-
-		<!--Menu Item-->
-		<tr>
 			<td>
 				<span class="table-title"><strong>Earl Grey Organic Black Tea & Bergamot</strong></span>
 				<div class="clearfix"></div>
 			</td>
 		</tr>
 
-		<!--Menu Item-->
 		<tr>
 			<td>
 				<span class="table-title"><strong>Berry Blueberry*</strong></span>
 				<div class="clearfix"></div>
 			</td>
-		</tr>
-
-		<!--Menu Item-->
-		<tr>
 			<td>
 				<span class="table-title"><strong>Ruby Slipper*</strong></span>
 				<div class="clearfix"></div>
 			</td>
 		</tr>
 
-		<!--Menu Item-->
 		<tr>
 			<td>
 				<span class="table-title"><strong>Strictly Slipper*</strong></span>
 				<div class="clearfix"></div>
 			</td>
-		</tr>
-
-		<!--Menu Item-->
-		<tr>
 			<td>
 				<span class="table-title"><strong>Eve's Temptation*</strong></span>
 				<div class="clearfix"></div>
 			</td>
 		</tr>
 
-		<!--Menu Item-->
 		<tr>
 			<td>
 				<span class="table-title"><strong>Really Raspberry*</strong></span>
 				<div class="clearfix"></div>
 			</td>
-		</tr>
-
-		<!--Menu Item-->
-		<tr>
 			<td>
 				<span class="table-title"><strong>Moontime Tea: Fertility Blend*</strong></span>
 				<div class="clearfix"></div>
 			</td>
 		</tr>
 
-		<!--Menu Item-->
 		<tr>
 			<td>
 				<span class="table-title"><strong>Peppermint</strong></span>
 				<div class="clearfix"></div>
 			</td>
-		</tr>
-
-		<!--Menu Item-->
-		<tr>
 			<td>
 				<span class="table-title"><strong>Lemongrass</strong></span>
 				<div class="clearfix"></div>
 			</td>
 		</tr>
 
-		<!--Menu Item-->
 		<tr>
 			<td>
 				<span class="table-title"><strong>Ginger</strong></span>
 				<div class="clearfix"></div>
 			</td>
-		</tr>
-
-		<!--Menu Item-->
-		<tr>
 			<td>
 				<span class="table-title"><strong>Chamomile</strong></span>
 				<div class="clearfix"></div>
 			</td>
 		</tr>
 
-		<!--Menu Item-->
 		<tr>
 			<td>
 				<span class="table-title"><strong>Remember Rosemary</strong></span>
 				<div class="clearfix"></div>
 				Chinese green tea, lemon myrtle, orange peel, rosemary
 			</td>
-		</tr>
-
-		<!--Menu Item-->
-		<tr>
 			<td>
 				<span class="table-title"><strong>Passion and Envy</strong></span>
 				<div class="clearfix"></div>
@@ -155,17 +116,12 @@
 			</td>
 		</tr>
 
-		<!--Menu Item-->
 		<tr>
 			<td>
 				<span class="table-title"><strong>Secret Garden Tisane</strong></span>
 				<div class="clearfix"></div>
 				Chamomile, lavender, peppermint, rooibos, rose petals
 			</td>
-		</tr>
-
-		<!--Menu Item-->
-		<tr>
 			<td>
 				<span class="table-title"><strong>Red Velvet *contains traces of sugar & dairy</strong></span>
 				<div class="clearfix"></div>
@@ -173,14 +129,174 @@
 			</td>
 		</tr>
 
-		<!--Menu Item-->
 		<tr>
 			<td>
 				<span class="table-title"><strong>Once Upon A Tea *contains traces of sugar & dairy</strong></span>
 				<div class="clearfix"></div>
 				Apple, cacao beans, chocolate bits, peppermint, rooibos, vanilla, white chocolate bits
 			</td>
+			<td>
+				<span class="table-title"><strong></strong></span>
+				<div class="clearfix"></div>
+			</td>
 		</tr>
+
+		<!--Menu Item-->
+		<!--<tr>
+			<td>
+				<span class="table-title"><strong>China Green</strong></span>
+				<div class="clearfix"></div>
+			</td>
+		</tr>
+-->
+		<!--Menu Item-->
+		<!--<tr>
+			<td>
+				<span class="table-title"><strong>Decaf Green*</strong></span>
+				<div class="clearfix"></div>
+			</td>
+		</tr>
+-->
+		<!--Menu Item-->
+		<!--<tr>
+			<td>
+				<span class="table-title"><strong>Pai Mu Tan White Tea</strong></span>
+				<div class="clearfix"></div>
+			</td>
+		</tr>
+-->
+		<!--Menu Item-->
+		<!--<tr>
+			<td>
+				<span class="table-title"><strong>Earl Grey Organic Black Tea & Bergamot</strong></span>
+				<div class="clearfix"></div>
+			</td>
+		</tr>
+-->
+		<!--Menu Item-->
+		<!--<tr>
+			<td>
+				<span class="table-title"><strong>Berry Blueberry*</strong></span>
+				<div class="clearfix"></div>
+			</td>
+		</tr>
+-->
+		<!--Menu Item-->
+		<!--<tr>
+			<td>
+				<span class="table-title"><strong>Ruby Slipper*</strong></span>
+				<div class="clearfix"></div>
+			</td>
+		</tr>
+-->
+		<!--Menu Item-->
+		<!--<tr>
+			<td>
+				<span class="table-title"><strong>Strictly Slipper*</strong></span>
+				<div class="clearfix"></div>
+			</td>
+		</tr>
+-->
+		<!--Menu Item-->
+		<!--<tr>
+			<td>
+				<span class="table-title"><strong>Eve's Temptation*</strong></span>
+				<div class="clearfix"></div>
+			</td>
+		</tr>
+-->
+		<!--Menu Item-->
+		<!--<tr>
+			<td>
+				<span class="table-title"><strong>Really Raspberry*</strong></span>
+				<div class="clearfix"></div>
+			</td>
+		</tr>
+-->
+		<!--Menu Item-->
+		<!--<tr>
+			<td>
+				<span class="table-title"><strong>Moontime Tea: Fertility Blend*</strong></span>
+				<div class="clearfix"></div>
+			</td>
+		</tr>
+-->
+		<!--Menu Item-->
+		<!--<tr>
+			<td>
+				<span class="table-title"><strong>Peppermint</strong></span>
+				<div class="clearfix"></div>
+			</td>
+		</tr>
+-->
+		<!--Menu Item-->
+		<!--<tr>
+			<td>
+				<span class="table-title"><strong>Lemongrass</strong></span>
+				<div class="clearfix"></div>
+			</td>
+		</tr>
+-->
+		<!--Menu Item-->
+		<!--<tr>
+			<td>
+				<span class="table-title"><strong>Ginger</strong></span>
+				<div class="clearfix"></div>
+			</td>
+		</tr>
+-->
+		<!--Menu Item-->
+		<!--<tr>
+			<td>
+				<span class="table-title"><strong>Chamomile</strong></span>
+				<div class="clearfix"></div>
+			</td>
+		</tr>
+-->
+		<!--Menu Item-->
+		<!--<tr>
+			<td>
+				<span class="table-title"><strong>Remember Rosemary</strong></span>
+				<div class="clearfix"></div>
+				Chinese green tea, lemon myrtle, orange peel, rosemary
+			</td>
+		</tr>-->
+
+		<!--Menu Item-->
+		<!--<tr>
+			<td>
+				<span class="table-title"><strong>Passion and Envy</strong></span>
+				<div class="clearfix"></div>
+				Blue mallow flowers, Chinese green tea, mango flavor, marigold petals, papaya flavor, pineapple flavor
+			</td>
+		</tr>-->
+
+		<!--Menu Item-->
+		<!--<tr>
+			<td>
+				<span class="table-title"><strong>Secret Garden Tisane</strong></span>
+				<div class="clearfix"></div>
+				Chamomile, lavender, peppermint, rooibos, rose petals
+			</td>
+		</tr>-->
+
+		<!--Menu Item-->
+		<!--<tr>
+			<td>
+				<span class="table-title"><strong>Red Velvet *contains traces of sugar & dairy</strong></span>
+				<div class="clearfix"></div>
+				Apple, cacao beans, chocolate bits, hibiscus, raspberry, rooibos, rose hips, vanilla, white chocolate bits
+			</td>
+		</tr>-->
+
+		<!--Menu Item-->
+		<!--<tr>
+			<td>
+				<span class="table-title"><strong>Once Upon A Tea *contains traces of sugar & dairy</strong></span>
+				<div class="clearfix"></div>
+				Apple, cacao beans, chocolate bits, peppermint, rooibos, vanilla, white chocolate bits
+			</td>
+		</tr>-->
 
 		</tbody>
 	</table>
