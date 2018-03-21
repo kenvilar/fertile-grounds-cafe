0120f5dfd3cfe6158b7ea6b52983ce1b53292ac4
Ken Vilar <kenvilar@gmail.com>
2018-03-22 02:23:06 +0800

    Change text domain

--- inc/custom-post-types/coffee-and-espresso.php
+++ inc/custom-post-types/coffee-and-espresso.php
@@ -2,40 +2,40 @@
 
 // Register Custom Post Type Coffee and Espresso
 // Post Type Key: coffeeandespresso
-function create_coffeeandespresso_cpt() {
+function fertilegrounds_coffeeandespresso_cpt() {
 	
 	$labels = array(
-		'name'                  => __( 'Coffee and Espresso', 'Post Type General Name', 'textdomain' ),
-		'singular_name'         => __( 'Coffee and Espresso', 'Post Type Singular Name', 'textdomain' ),
-		'menu_name'             => __( 'Coffee and Espresso', 'textdomain' ),
-		'name_admin_bar'        => __( 'Coffee and Espresso', 'textdomain' ),
-		'archives'              => __( 'Coffee and Espresso Archives', 'textdomain' ),
-		'attributes'            => __( 'Coffee and Espresso Attributes', 'textdomain' ),
-		'parent_item_colon'     => __( 'Parent Coffee and Espresso:', 'textdomain' ),
-		'all_items'             => __( 'All Coffee and Espresso', 'textdomain' ),
-		'add_new_item'          => __( 'Add New Coffee and Espresso', 'textdomain' ),
-		'add_new'               => __( 'Add New', 'textdomain' ),
-		'new_item'              => __( 'New Coffee and Espresso', 'textdomain' ),
-		'edit_item'             => __( 'Edit Coffee and Espresso', 'textdomain' ),
-		'update_item'           => __( 'Update Coffee and Espresso', 'textdomain' ),
-		'view_item'             => __( 'View Coffee and Espresso', 'textdomain' ),
-		'view_items'            => __( 'View Coffee and Espresso', 'textdomain' ),
-		'search_items'          => __( 'Search Coffee and Espresso', 'textdomain' ),
-		'not_found'             => __( 'Not found', 'textdomain' ),
-		'not_found_in_trash'    => __( 'Not found in Trash', 'textdomain' ),
-		'featured_image'        => __( 'Featured Image', 'textdomain' ),
-		'set_featured_image'    => __( 'Set featured image', 'textdomain' ),
-		'remove_featured_image' => __( 'Remove featured image', 'textdomain' ),
-		'use_featured_image'    => __( 'Use as featured image', 'textdomain' ),
-		'insert_into_item'      => __( 'Insert into Coffee and Espresso', 'textdomain' ),
-		'uploaded_to_this_item' => __( 'Uploaded to this Coffee and Espresso', 'textdomain' ),
-		'items_list'            => __( 'Coffee and Espresso list', 'textdomain' ),
-		'items_list_navigation' => __( 'Coffee and Espresso list navigation', 'textdomain' ),
-		'filter_items_list'     => __( 'Filter Coffee and Espresso list', 'textdomain' ),
+		'name'                  => __( 'Coffee and Espresso', 'Post Type General Name', 'fertilegroundscafe' ),
+		'singular_name'         => __( 'Coffee and Espresso', 'Post Type Singular Name', 'fertilegroundscafe' ),
+		'menu_name'             => __( 'Coffee and Espresso', 'fertilegroundscafe' ),
+		'name_admin_bar'        => __( 'Coffee and Espresso', 'fertilegroundscafe' ),
+		'archives'              => __( 'Coffee and Espresso Archives', 'fertilegroundscafe' ),
+		'attributes'            => __( 'Coffee and Espresso Attributes', 'fertilegroundscafe' ),
+		'parent_item_colon'     => __( 'Parent Coffee and Espresso:', 'fertilegroundscafe' ),
+		'all_items'             => __( 'All Coffee and Espresso', 'fertilegroundscafe' ),
+		'add_new_item'          => __( 'Add New Coffee and Espresso', 'fertilegroundscafe' ),
+		'add_new'               => __( 'Add New', 'fertilegroundscafe' ),
+		'new_item'              => __( 'New Coffee and Espresso', 'fertilegroundscafe' ),
+		'edit_item'             => __( 'Edit Coffee and Espresso', 'fertilegroundscafe' ),
+		'update_item'           => __( 'Update Coffee and Espresso', 'fertilegroundscafe' ),
+		'view_item'             => __( 'View Coffee and Espresso', 'fertilegroundscafe' ),
+		'view_items'            => __( 'View Coffee and Espresso', 'fertilegroundscafe' ),
+		'search_items'          => __( 'Search Coffee and Espresso', 'fertilegroundscafe' ),
+		'not_found'             => __( 'Not found', 'fertilegroundscafe' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'fertilegroundscafe' ),
+		'featured_image'        => __( 'Featured Image', 'fertilegroundscafe' ),
+		'set_featured_image'    => __( 'Set featured image', 'fertilegroundscafe' ),
+		'remove_featured_image' => __( 'Remove featured image', 'fertilegroundscafe' ),
+		'use_featured_image'    => __( 'Use as featured image', 'fertilegroundscafe' ),
+		'insert_into_item'      => __( 'Insert into Coffee and Espresso', 'fertilegroundscafe' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this Coffee and Espresso', 'fertilegroundscafe' ),
+		'items_list'            => __( 'Coffee and Espresso list', 'fertilegroundscafe' ),
+		'items_list_navigation' => __( 'Coffee and Espresso list navigation', 'fertilegroundscafe' ),
+		'filter_items_list'     => __( 'Filter Coffee and Espresso list', 'fertilegroundscafe' ),
 	);
 	$args   = array(
-		'label'               => __( 'Coffee and Espresso', 'textdomain' ),
-		'description'         => __( '', 'textdomain' ),
+		'label'               => __( 'Coffee and Espresso', 'fertilegroundscafe' ),
+		'description'         => __( '', 'fertilegroundscafe' ),
 		'labels'              => $labels,
 		'menu_icon'           => 'dashicons-heart',
 		'supports'            => array( 'title', 'revisions', 'page-attributes', 'post-formats', ),
@@ -58,4 +58,4 @@ function create_coffeeandespresso_cpt() {
 	
 }
 
-add_action( 'init', 'create_coffeeandespresso_cpt', 0 );
+add_action( 'init', 'fertilegrounds_coffeeandespresso_cpt', 0 );
