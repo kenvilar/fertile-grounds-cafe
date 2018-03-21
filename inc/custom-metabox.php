d221fed819d7f33088fe21e23b7ce293ff2cb277
Ken Vilar <kenvilar@gmail.com>
2018-03-22 03:09:40 +0800

    Add custom size metabox for specialty drink post type

--- inc/custom-metabox.php
+++ inc/custom-metabox.php
@@ -8,14 +8,14 @@ include get_template_directory() . '/inc/custom-metabox/coffee-and-espresso/Coff
 /**
  * For Specialty Drink Post Type
  */
-//include get_template_directory() . '/inc/custom-post-types/specialty-drinks.php';
+include get_template_directory() . '/inc/custom-metabox/specialty-drink/SpecialtyDrinkSizesMetabox.php';
 
 /**
  * For Teas and Tisane Post Type
  */
-//include get_template_directory() . '/inc/custom-post-types/teas-and-tisane.php';
+//include get_template_directory() . '/inc/custom-metabox/specialty-drink/SpecialtyDrinkSizesMetabox.php';
 
 /**
  * For Smoothies Post Type
  */
-//include get_template_directory() . '/inc/custom-post-types/smoothies.php';
+//include get_template_directory() . '/inc/custom-metabox/specialty-drink/SpecialtyDrinkSizesMetabox.php';
